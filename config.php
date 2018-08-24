445a136ef55181e1e1ca0356ac9bb1513a615b79
SpaceDerp <Nathanxaviles@gmail.com>
2018-08-23 23:04:10 -0400

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+ /* An XML database can easily be read, therefore, private information should be set here */
+	
+	$user = "Space"; // 
+	$passw = "Blueboy12"; // 
+	$apikey = "22216A1FC46909C8C8D7E13C2F44617A"; // Find your API Key at https://steamcommunity.com/dev/apikey
+
+?>
